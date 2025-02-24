d20b7283d4cc74f036b265504a8fa0d0cc31a4ff
Raniel Sobrinho <raniel.sobrinho@g4flex.com.br>
2025-02-24 11:42:22 -0300

    refactor: use todo component form and use todo store function to remove todo from list

--- src/App.vue
+++ src/App.vue
@@ -1,103 +1,28 @@
 <script setup>
 import { ref, onMounted, computed, watch } from 'vue'
+import TodoForm from './components/form/todoForm.vue'
+import { useTodoStore } from './stores/TodoStore.js'
 
-const todos = ref([])
-const name = ref('')
-
-const input_content = ref('')
-const input_category = ref(null)
-
-const todos_asc = computed(() =>
-  todos.value.sort((a, b) => {
-    return b.createdAt - a.createdAt
-  })
-)
-
-const addTodo = () => {
-  if (input_content.value.trim() === '' || input_category.value === null) {
-    return
-  }
-
-  todos.value.push({
-    content: input_content.value,
-    category: input_category.value,
-    done: false,
-    createdAt: new Date().getTime()
-  })
-
-  input_category.value = null
-  input_content.value = ''
-}
+const todoStore = useTodoStore()
 
 const removeTodo = (todo) => {
-  todos.value = todos.value.filter((t) => t !== todo)
+  todoStore.removeTodo(todo)
 }
-
-watch(name, (newVal) => {
-  localStorage.setItem('name', newVal)
-})
-
-watch(
-  todos,
-  (newVal) => {
-    localStorage.setItem('todos', JSON.stringify(newVal))
-  },
-  { deep: true }
-)
-
-onMounted(() => {
-  name.value = localStorage.getItem('name') || ''
-  todos.value = JSON.parse(localStorage.getItem('todos') || '[]')
-})
 </script>
 
 <template>
   <main class="app">
     <section class="greeting">
-      <h2 class="title">
-        What's up, <input type="text" placeholder="Name here" v-model="name" />
-      </h2>
+      <h2 class="title">Sua lista de tarefas</h2>
     </section>
     <section class="create-todo">
-      <h3 class="create-todo">Criar tarefa</h3>
-      <form @submit.prevent="addTodo">
-        <h4>What's on your todo list?</h4>
-        <input
-          type="text"
-          placeholder="e.g. make a video"
-          v-model="input_content"
-        />
-        <h4>Pick a category</h4>
-        <div class="options">
-          <label>
-            <input
-              type="radio"
-              name="category"
-              value="business"
-              v-model="input_category"
-            />
-            <span class="bubble business"></span>
-            <div>Business</div>
-          </label>
-          <label>
-            <input
-              type="radio"
-              name="category"
-              value="personal"
-              v-model="input_category"
-            />
-            <span class="bubble personal"></span>
-            <div>Personal</div>
-          </label>
-        </div>
-        <button type="submit">Criar tarefa</button>
-      </form>
+      <TodoForm />
     </section>
     <section class="todo-list">
       <h3>TODO LIST</h3>
       <div class="list">
         <div
-          v-for="todo in todos_asc"
+          v-for="todo in todoStore.todos"
           :class="`todo-item ${todo.done && 'done'}`"
         >
           <label>
