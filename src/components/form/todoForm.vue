8a75bfdb45960b445cd040af89ed9b5750d7de4c
Raniel Sobrinho <raniel.sobrinho@g4flex.com.br>
2025-03-01 18:41:06 -0300

    refactor: remove coments

--- src/components/form/todoForm.vue
+++ src/components/form/todoForm.vue
@@ -38,7 +38,6 @@ const closeModal = () => {
       align-items: center;
     "
   >
-    <!-- <h4>What's on your todo list?</h4> -->
     <v-text-field
       label="Nova tarefa"
       variant="solo-filled"
@@ -47,40 +46,12 @@ const closeModal = () => {
       style="margin: auto"
       v-model="input_content"
     ></v-text-field>
-    <!-- <input -->
-    <!--   type="text" -->
-    <!--   placeholder="e.g. make a video" -->
-    <!--   v-model="input_content" -->
-    <!-- /> -->
     <h4>Escolha uma categoria</h4>
     <v-radio-group inline>
       <v-radio label="Negócios" value="business"></v-radio>
       <v-radio label="Estudos" value="study"></v-radio>
       <v-radio label="Pessoal" value="personal"></v-radio>
     </v-radio-group>
-    <!-- <div class="options"> -->
-    <!--   <label> -->
-    <!--     <input -->
-    <!--       type="radio" -->
-    <!--       name="category" -->
-    <!--       value="business" -->
-    <!--       v-model="input_category" -->
-    <!--     /> -->
-    <!--     <span class="bubble business"></span> -->
-    <!--     <div>Business</div> -->
-    <!--   </label> -->
-    <!--   <label> -->
-    <!--     <input -->
-    <!--       type="radio" -->
-    <!--       name="category" -->
-    <!--       value="personal" -->
-    <!--       v-model="input_category" -->
-    <!--     /> -->
-    <!--     <span class="bubble personal"></span> -->
-    <!--     <div>Personal</div> -->
-    <!--   </label> -->
-    <!-- </div> -->
-    <!-- <button type="submit">Criar tarefa</button> -->
     <div>
       <v-btn variant="tonal" prepend-icon="mdi-plus-circle" type="submit"
         >Criar tarefa</v-btn
