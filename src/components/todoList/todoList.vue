8ad0fcbb57126402c39d2ba647a01c878da21e71
Raniel Sobrinho <raniel.sobrinho@g4flex.com.br>
2025-03-01 18:40:44 -0300

    feat: create new component to handle todos visualization

---
+++ src/components/todoList/todoList.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { ref, onMounted, computed, watch } from 'vue'
+import { useTodoStore } from '../../stores/TodoStore.js'
+
+const todoStore = useTodoStore()
+
+const removeTodo = (todo) => {
+  todoStore.removeTodo(todo)
+}
+</script>
+
+<template>
+  <div class="list">
+    <div
+      v-for="todo in todoStore.todos"
+      :class="`todo-item ${todo.done && 'done'}`"
+    >
+      <label>
+        <input type="checkbox" v-model="todo.done" />
+        <span :class="`bubble ${todo.category}`"></span>
+
+        <div class="todo-content">
+          <input type="text" v-model="todo.content" />
+        </div>
+
+        <div class="actions">
+          <button class="delete" @click="removeTodo(todo)">Delete</button>
+        </div>
+      </label>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
