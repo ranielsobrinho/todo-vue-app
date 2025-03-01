d943b0ac5e551f07e31e9ba7475a15fb15ecae60
Raniel Sobrinho <raniel.sobrinho@g4flex.com.br>
2025-03-01 18:39:52 -0300

    chore: add vuetify to project

--- src/main.js
+++ src/main.js
@@ -1,6 +1,21 @@
 import { createApp } from 'vue'
-import './style.css'
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import '@mdi/font/css/materialdesignicons.css'
 import App from './App.vue'
 import { createPinia } from 'pinia'
 
-createApp(App).use(createPinia()).mount('#app')
+const vuetify = createVuetify({
+  icons: {
+    defaultSet: 'mdi'
+  },
+  components,
+  directives,
+  theme: {
+    defaultTheme: 'dark'
+  }
+})
+
+createApp(App).use(vuetify).use(createPinia()).mount('#app')
